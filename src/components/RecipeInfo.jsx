6e781cb05722fce43c3c67ddc97fa91c1de4f7f5
Rohan <rohanshikhare410@gmail.com>
2024-06-20 00:10:06 +0530

    all done

--- src/components/RecipeInfo.jsx
+++ src/components/RecipeInfo.jsx
@@ -8,7 +8,7 @@ const RecipeInfo=()=> {
   const {MealId}=useParams();
   
   if(MealId!=""){
-    fetch(`https:/www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`).then(res=>res.json())
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`).then(res=>res.json())
     .then(data=>{
       setItem(data.meals[0]);
     })
