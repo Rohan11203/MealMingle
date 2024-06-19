3ebc96343dfb560716ee6ff178be55d78dbe1579
Rohan <rohanshikhare410@gmail.com>
2024-06-19 23:49:57 +0530

    kk

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import CustomImage from "./CustomImage";
 
-export default function HeroSection(){
+export default function HeroSection() {
   const images = [
     "/img/gallery/img_1.jpg",
     "/img/gallery/img_2.jpg",
@@ -14,23 +14,28 @@ export default function HeroSection(){
     "/img/gallery/img_9.jpg",
   ];
   const navigate = useNavigate();
-  const exploreSearch=()=>{
-    navigate('/recipes')
-  }
+  const exploreSearch = () => {
+    navigate("/recipes");
+  };
   return (
     <div className="section hero">
       <div className="col typography">
         <h1 className="title">What are we About</h1>
-        <p className="info">some information about the Recipe Api, Build in 2024 may 23. In this recipe search You can search any recipe you want to make.</p>
+        <p className="info">
+          some information about the Recipe Api, Build in 2024 may 23. In this
+          recipe search You can search any recipe you want to make.
+        </p>
 
-        <button onClick={exploreSearch} className="btn">Explore Now</button>
+        <button onClick={exploreSearch} className="btn">
+          Explore Now
+        </button>
       </div>
 
       <div className="col gallery">
-        { images.map((src,index) =>(
-          <CustomImage key={index} imgSrc = {src} pt={("85%")} />
-        )) }
+        {images.map((src, index) => (
+          <CustomImage key={index} imgSrc={src} pt={"85%"} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom"
-export default function Navbar(){
+import { Link } from "react-router-dom";
+import Recipes from "../pages/Recipes";
+export default function Navbar() {
   return (
-  <div className="navbar container">
-    <a className="logo" href="/">M<span>ea</span>l<span>M</span>ingle</a>
-    <div className="nav-links">
-
-      <a className="home" href="/">Home</a>
-      <a  href="recipes">Recipes</a>
-      <a className="home" href="#contact-us">Contact Us</a>
+    <div className="navbar container">
+      <Link to="">
+        M<span>ea</span>l<span>M</span>ingle
+      </Link>
+      
+      <Link className="logo" to=""></Link>
+      <div className="nav-links">
+        <a className="home" href="/">
+          Home
+        </a>
+        <Link to="recipes">Recipes</Link>
+        <a className="home" href="#contact-us">
+          Contact Us
+        </a>
+      </div>
     </div>
-  </div>
-)
+  );
 }
